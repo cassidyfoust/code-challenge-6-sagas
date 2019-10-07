91de566e6e20e81c83ee842e95f7508af3fed96b
Cassidy Foust <cassidy.foust@gmail.com>
2019-10-07 13:11:41 -0500

    added sql query

--- server/routes/zoo.router.js
+++ server/routes/zoo.router.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 router.get('/', (req, res) => {
-    // YOUR CODE HERE
-    
+    const queryText = `SELECT "species".species_name, "class".class_name FROM "species"
+JOIN "class"
+	ON "class".id = "species".class_id;`
+    pool.query(queryText)
+        .then((result) => { res.send(result.rows); })
+        .catch((err) => {
+            console.log('Error completing species and animals query', err);
+            res.sendStatus(500);
+        });
 });
 
 module.exports = router;
